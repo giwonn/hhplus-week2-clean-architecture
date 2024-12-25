90736888b114e925cbd9acbd3c8ca843dc1cbbf8
giwonn <l4279625@gmail.com>
2024-12-25 15:56:08 +0900

    refactor: DB 세팅 및 테스트 클래스명 변경

--- src/test/java/com/example/hhplus/HhplusApplicationTests.java
+++ src/test/java/com/example/hhplus/HhplusApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class HhplusWeek2CleanArchitectureApplicationTests {
+class HhplusApplicationTests {
 
 	@Test
 	void contextLoads() {
